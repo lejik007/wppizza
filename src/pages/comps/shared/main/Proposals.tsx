eaf2015fd13cfba4582091c7000260f9626e5895
lejik <lejik007@gmail.com>
2023-08-11 05:59:27 +0300

    All useless comments were deleted

--- src/pages/comps/shared/main/Proposals.tsx
+++ src/pages/comps/shared/main/Proposals.tsx
@@ -5,22 +5,6 @@ export default function Proposals() {
     return (
         <div className="Proposals">
             <ProposalsLeft/>
-            {/*<div className="ProposalsLeft">*/}
-            {/*    <div className="ProposalsLeftImage">*/}
-            {/*        <div className="ProposalsLeftImageLeft">*/}
-
-            {/*        </div>*/}
-            {/*        <div className="ProposalsLeftImageRight">*/}
-
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*    <p className="ProposalsCaption">*/}
-            {/*        Закажи 2 пиццы – 3-я в подарок*/}
-            {/*    </p>*/}
-            {/*    <p className="ProposalsDiscreption">*/}
-            {/*        При заказе 2-х больших пицц – средняя пицца в подарок*/}
-            {/*    </p>*/}
-            {/*</div>*/}
             <div className="ProposalsCenter">
                 <div className="ProposalsCenterImage">
 
